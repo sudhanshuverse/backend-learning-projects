5c8b098054e6e71c5fc481b6ae168c513931e584
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-17 11:02:17 +0530

    Added the all schema

--- 02-todo-list/models/Todo.js
+++ 02-todo-list/models/Todo.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
-    task: String,
-    required: true
-})
+    task: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
 
-const Todo = mongoose.model("Note", TodoSchema);
-
-module.exports = Todo;
+const Todo = mongoose.model('Todo', TodoSchema);
+module.exports = Todo; 
