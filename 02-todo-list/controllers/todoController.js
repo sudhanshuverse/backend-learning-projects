e0f42c0adf1effd20ab80ff4340cbe71292145d8
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-17 11:01:38 +0530

    Added the all controlling methods

--- 02-todo-list/controllers/todoController.js
+++ 02-todo-list/controllers/todoController.js
@@ -1,6 +1,37 @@
-const Note = require('../models/Todo');
+const Todo = require('../models/Todo');
 
+// Get all tasks
 exports.getTodo = async (req, res) => {
-    const todos = await Note.find();
-    res.render("home", { todos });
+    try {
+        const todos = await Todo.find();
+        res.render("home", { todos });
+    } catch (err) {
+        res.status(500).send("Error fetching todos");
+    }
+};
+
+// Save task
+exports.createTodo = async (req, res) => {
+    const { task } = req.body;
+    await Todo.create({ task });
+    res.redirect("/");
+};
+
+// Task status
+exports.toggleStatus = async (req, res) => {
+    const id = req.params.id;
+    const todo = await Todo.findById(id);
+    await Todo.findByIdAndUpdate(id, {
+        status: !todo.status
+    });
+
+    res.redirect("/");
+};
+
+// Delete task
+exports.deleteTodo = async (req, res) => {
+    const id = req.params.id;
+    await Todo.findByIdAndDelete(id);
+    res.redirect("/");
 }
+
