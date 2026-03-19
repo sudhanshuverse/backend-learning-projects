eb958f35a4edfcfdb58ac7bbb688750bce701013
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-19 16:35:04 +0530

    Added schema to handle the databse operations

---
+++ 06-blog-app/models/Blog.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+},{timestamps:  true});
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
