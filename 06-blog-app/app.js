579a3b6c1e1cf920415184ff37941ae806b65146
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-19 16:37:11 +0530

    Added code to stablish the connection with databse

---
+++ 06-blog-app/app.js
@@ -0,0 +1,31 @@
+// External module
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+
+// Local module
+const blogRouter = require("./routers/blogRouter");
+
+// Mongo URL
+const DB_PATH = "mongodb://root:root@ac-u0nafti-shard-00-00.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-01.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-02.r9jtfbr.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-dvojmu-shard-0&authSource=admin&appName=Practice";
+const app = express();
+
+// view engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.static('public'));   // Access the static file
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", blogRouter)
+
+
+const PORT = 8000;
+mongoose.connect(DB_PATH)
+    .then(() => {
+        console.log(`Mongodb is successfully connected`);
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        })
+    }).catch(err => {
+        console.log('Error while connecting to database');
+    })
