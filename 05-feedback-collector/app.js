9cdbe5d96a243f0fad7da37f5fc58d85b78c6b5a
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-03-19 14:11:05 +0530

    connect to database

---
+++ 05-feedback-collector/app.js
@@ -0,0 +1,32 @@
+// External module
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+
+// Mongoose URl
+const DB_PATH = "mongodb://root:root@ac-u0nafti-shard-00-00.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-01.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-02.r9jtfbr.mongodb.net:27017/feedback?ssl=true&replicaSet=atlas-dvojmu-shard-0&authSource=admin&appName=Practice";
+
+// view engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.static('public'))  // Access the static files
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req, res) => {
+    res.send("<h1>Hello, how are you</h1>")
+})
+
+
+const PORT = 8000;
+mongoose.connect(DB_PATH)
+    .then(() => {
+        console.log("Mongodb is successfully connected");
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        })
+    }).catch(err => {
+        console.log('Error white connecting to mongo', err);
+    })
