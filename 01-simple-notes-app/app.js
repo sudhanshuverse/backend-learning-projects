09061f4d196873524492b5c2355326fb928518b4
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-04-05 23:14:57 +0530

    Update connection url

--- 01-simple-notes-app/app.js
+++ 01-simple-notes-app/app.js
@@ -1,7 +1,7 @@
 // External module
 const express = require('express');
 const { default: mongoose } = require('mongoose');
-const DB_PATH = "mongodb://root:root@ac-u0nafti-shard-00-00.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-01.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-02.r9jtfbr.mongodb.net:27017/notes?ssl=true&replicaSet=atlas-dvojmu-shard-0&authSource=admin&appName=Practice";
+const DB_PATH = "mongodb://localhost:27017//notes";
 
 // Local module
 const noteRouters = require('./routes/noteRoutes');
