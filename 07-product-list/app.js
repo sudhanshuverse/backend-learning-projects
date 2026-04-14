646e823c89f493a5e2c4982e5a05e356667249ff
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-04-14 20:08:40 +0530

    Remove the cluster URl with local mongodb

--- 07-product-list/app.js
+++ 07-product-list/app.js
@@ -7,7 +7,7 @@ const { default: mongoose } = require('mongoose');
 const productRouter = require('./routers/productRouter');
 
 // Mongo URL
-const DB_PATH = "mongodb://root:root@ac-u0nafti-shard-00-00.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-01.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-02.r9jtfbr.mongodb.net:27017/product?ssl=true&replicaSet=atlas-dvojmu-shard-0&authSource=admin&appName=Practice";
+const DB_PATH = "mongodb://localhost:27017//product";
 const app = express();
 
 // view engine
