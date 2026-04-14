d5c46e80b1c6dd95fdb6000e8fcaa4ca56381d05
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-04-14 20:04:59 +0530

    Remove the cluster URl with local mongodb

--- 02-todo-list/app.js
+++ 02-todo-list/app.js
@@ -6,7 +6,7 @@ const { default: mongoose } = require('mongoose');
 const todoRoutes = require('./routes/todoRoutes');
 
 // Mongodb url
-const DB_PATH = "mongodb://root:root@ac-u0nafti-shard-00-00.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-01.r9jtfbr.mongodb.net:27017,ac-u0nafti-shard-00-02.r9jtfbr.mongodb.net:27017/todo?ssl=true&replicaSet=atlas-dvojmu-shard-0&authSource=admin&appName=Practice";
+const DB_PATH = "mongodb://localhost:27017//todo";
 
 
 const app = express();
